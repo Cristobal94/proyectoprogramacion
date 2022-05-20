43314053e1ec8cd09a2c1800090299d479067c3e
DaddyDarky <darknessluismanuel999@gmail.com>
2022-05-20 09:12:33 +0200

    subida L&B

---
+++ src/vista/reservas/Reserva.java
@@ -0,0 +1,41 @@
+package vista.reservas;
+
+import javax.swing.*;
+
+public class Reserva {
+    private JFrame ventanaReservas;
+    private JPanel panelPrincipal;
+    private JLabel labelPrueba;
+    private JPanel panelInferior;
+    private JButton botonSalir;
+    private JButton botonCerrarSesion;
+    private JButton buttonGuardar;
+    private JButton buttonCargar;
+    private JTextField reservasTextField;
+
+    public Reserva () {
+        ventanaReservas = new JFrame("Aplicación reservas");
+        ventanaReservas.setContentPane(panelPrincipal);
+        ventanaReservas.setSize(800,800);
+        ventanaReservas.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventanaReservas.setLocationRelativeTo(null);
+        // ventanaLogin.pack();
+        //ventanaReservas.setVisible(true);
+    }
+
+    public JFrame getVentanaReservas() {
+        return ventanaReservas;
+    }
+
+    public JLabel getLabelPrueba() {
+        return labelPrueba;
+    }
+
+    public static void main(String[] args) {
+        new Reserva();
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
